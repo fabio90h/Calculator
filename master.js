1a0ec15ef73c3bd1b6f4fef6bf59f70480ad18e5
Fabio Hsu <Fabio90h@Fabios-MBP.attlocal.net>
2018-04-10 21:03:23 -0400

    Adding all the files

---
+++ master.js
@@ -0,0 +1,33 @@
+
+var results = document.querySelector("#result"); //display the result
+var current = document.querySelector("#current"); //display what was pressed
+
+var construct = ""; //to display
+var total = ""; //result int number;
+
+var buttons = document.querySelectorAll('button');
+var buttonsCount = buttons.length;
+for (var i = 0; i < buttonsCount; i += 1) {
+    buttons[i].onclick = function() {
+    var target = document.querySelector("#" + this.id).textContent;
+    if (target !== "CLR" && target !== "="){
+      total += target;
+      current.style.cssText = "color: black";
+      results.style.cssText = "color: silver";
+      current.textContent = total;
+      results.textContent = eval(total);
+    }
+    else if (target === "CLR") {
+      total = "";
+      current.textContent = "World";
+      results.textContent = "Hello";
+      current.style.cssText = "color: white";
+      results.style.cssText = "color: white";
+    }else if (target === "=") {
+      var completeTotal = eval(total);
+      total = completeTotal;
+      current.textContent = total;
+      results.style.cssText = "color: white";
+    }
+  };
+};
